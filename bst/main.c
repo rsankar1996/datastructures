ec338b0fa12d7dca841f3c9e34387d5a7512a615
Rakesh S <s.rakesh@outlook.in>
2026-06-12 21:42:52 +0530

    added binary tree traversal functions
    Signed-off-by: Rakesh S <s.rakesh@outlook.in>

--- bst/main.c
+++ bst/main.c
@@ -1,69 +1,75 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct bst
+typedef struct btree
 {
-	int data;
-	struct bst * left;
-	struct bst * right;
-} bst_t;
+	int key;
+	struct btree * lchild;
+	struct btree * rchild;
+} btree_t;
 
-void bst_traverse(bst_t * root)
-{
-	bst_t * node = root;
-	
-	if (NULL != node->left)
-	{
-		bst_traverse (node->left);
-	}
-	
-	printf("%d ", node->data);
-	
-	if (NULL != node->right)
-	{
-		bst_traverse (node->right);
-	}
-}
+/*
+Pre-order traversal 
+---------------------------
+1. Visit the root node
+2. Traverse the left subtree of root node in preorder (L)
+3. Traverse the right subtree of root node in preorder (R)
+*/
 
-bst_t * add_node (int data)
-{
-	bst_t * root = (bst_t *) malloc (sizeof (bst_t));
-	
-	root->data = data;
-	root->left = NULL;
-	root->right = NULL;
-	
-	return root;
-}
-
-bst_t * insert_node (bst_t * root, int data)
+void pre_order_trv (btree_t * root)
 {
 	if (NULL == root)
 	{
-		return add_node (data);
+		return;
 	}
 	
-	if (data < (root->data))
+	printf("%d ", root->key);
+	
+	pre_order_trv(root->lchild);
+	pre_order_trv(root->rchild);
+}
+
+/*
+Inorder traversal
+-----------------------
+1. Traverse the left subtree of root node in in-order (L)
+2. Visit the root
+3. Traverse the right subtree of root node in in-order (R)
+*/
+
+void in_order_trv (btree_t * root)
+{
+	if (NULL == root)
 	{
-		root->left = insert_node (root->left, data);
-	}
-	else if (data > (root->data))
-	{
-		root->right = insert_node (root->right, data);
+		return;
 	}
 	
-	return root;
+	in_order_trv(root->lchild);
+	printf("%d ", root->key);
+	in_order_trv(root->rchild);
+}
+
+/*
+Post order traversal
+---------------------------
+1. Traverse the left subtree of root node in post-order (L)
+2. Traverse the right subtree of root node in in-order (R)
+3. Visit the root node
+*/
+void post_order_trv (btree_t * root)
+{
+	if (NULL == root)
+	{
+		return;
+	}
+	
+	post_order_trv(root->lchild);
+	printf("%d ", root->key);
+	post_order_trv(root->rchild);
 }
 
 int main()
 {
-	bst_t * root = add_node(10);
-	
-	bst_traverse(root);
-	
-	root = insert_node (root, 20);
-	
-	bst_traverse(root);
 
 	return 0;
 }
