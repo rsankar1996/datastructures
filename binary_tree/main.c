09f7b36859890088bfb27a61ed4537bf5c9af89c
Rakesh S <s.rakesh@outlook.in>
2026-06-19 21:03:34 +0530

    added level order traversal in binary tree
    Signed-off-by: Rakesh S <s.rakesh@outlook.in>

--- binary_tree/main.c
+++ binary_tree/main.c
@@ -68,6 +68,62 @@ void post_order_trv (btree_t * root)
 	post_order_trv(root->rchild);
 }
 
+/* 
+Level order traversal
+---------------------------------
+*/
+
+int height(btree_t* root) 
+{
+	if (root == NULL)
+    	{
+    		return 0;
+    	}
+
+    	int leftHeight = height(root->left);
+    	int rightHeight = height(root->right);
+    	
+    	return max(leftHeight,rightHeight) + 1;
+}
+
+void print_given_level(btree_t* root, int level)
+{
+    	if (root == NULL)
+    	{
+    		return;
+    	}
+    	
+    	if (level > 1)
+    	{
+    		print_given_level (root->left, (level - 1));
+    		print_given_level (root->right, (level - 1));
+    	}
+    	else if (level == 1)
+    	{
+    		printf ("%d ", root->data);
+    	}
+}
+
+void level_order_traversal(btree_t* root)
+{
+    	int h = height(root);
+    	for (int i = 1; i <= h; i++)
+    	{
+        	print_given_level(root, i);
+    	}
+}
+
+/* 
+Level order traversal using queue
+----------------------------------------------
+1. Push the root node into queue
+2. Loop the queue until queue empty
+3. Pop the queue
+4. Push the left node into queue if available
+5. Push the right node into queue if available
+6. Repeat the step 2.
+*/
+
 int main()
 {
 
